e9f24e183984ba42192913e954cc9442244c58bd
Amin Bagheri <amin.bagheri@gmail.com>
2022-09-30 12:06:41 -0400

    Initial Commit

---
+++ Restr/RestrApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestrApp.swift
+//  Restr
+//
+//  Created by Amin  Bagheri  on 2022-09-30.
+//
+
+import SwiftUI
+
+@main
+struct RestrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
